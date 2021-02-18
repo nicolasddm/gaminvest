b5be7ced4ba85b848d6dde27b03471785caac243
nicolasddm <nicolasdenckerdemarco@gmail.com>
2021-02-18 08:35:09 -0300

    Adicionar página Sobre

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
+import Link from 'next/link';
+
 function Home() {
-    return <div>Página Inicial</div>
+    return (<div>
+            <h1>Home</h1>
+        
+            <Link href="/sobre">
+                <a>Acessar página Sobre</a>
+            </Link>
+    </div>)
 }
 
 export default Home
